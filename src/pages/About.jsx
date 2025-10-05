d750be33c06ee217d5d8f15bbf1967e32e5f908b
mattz1992 <shizumaru.matz@gmail.com>
2025-10-05 13:19:09 -0300

    abou

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,8 +2,23 @@ import { Container } from "react-bootstrap";
 export default function About(){
   return (
     <Container className="py-4">
-      <h1 className="h4">Sobre el proyecto</h1>
-      <p>SPA de ejemplo con React, CRUD en Firebase Firestore, filtros y paginación. Tema: tienda de regalos, chocolate y peluches.</p>
+      <h2>Acerca de Nosotros</h2>
+    <p>
+        En <strong>Desayunos Romana</strong>, nuestra pasión es simple: <strong>entregar sonrisas y el mejor sabor</strong> a domicilio.
+    </p>
+    <p>
+        Somos especialistas en crear <strong>momentos inolvidables</strong> a través de nuestros servicios:
+    </p>
+    <ul>
+        <li><strong>Desayunos a Domicilio:</strong> Frescura, calidad e ingredientes seleccionados para empezar el día de forma mágica.</li>
+        <li><strong>Regalos y Chocolates:</strong> Detalles únicos y el más exquisito chocolate artesanal para celebrar cualquier ocasión.</li>
+    </ul>
+    <p>
+        Cuidamos cada detalle para que tu sorpresa sea perfecta.
+    </p>
+    <p>
+        <strong>Desayunos Romana:</strong> La forma más deliciosa de decir "te quiero" o "feliz día".
+    </p>
     </Container>
   );
 }
